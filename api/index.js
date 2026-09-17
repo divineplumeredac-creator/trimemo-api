9cd7ca5578bf3cf0d0f60db35fec1b5a46efcb69
divineplumeredac-creator <divineplumeredac@gmail.com>
2026-09-17 02:13:00 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
   res.status(200).json({
     name: 'Trimémo Academic Engine',
     version: '1.0',
